4d6143c22e252098f4c96c19e880b148d32389cc
Vanlightly <vanlightly@gmail.com>
2018-11-20 18:56:10 +0100

    Added Connection Killer class

---
+++ IntegrationTesting/RabbitMQTestExamples/RabbitMQTestExamples.IntegrationTests/Helpers/ConnectionKiller.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json.Linq;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQTestExamples.IntegrationTests.Helpers
+{
+    public class ConnectionKiller
+    {
+        private static HttpClient HttpClient;
+
+        public static void Initialize(string nodeUrl, string username, string password)
+        {
+            if (HttpClient == null)
+            {
+                var authValue = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}")));
+
+                HttpClient = new HttpClient();
+                HttpClient.BaseAddress = new Uri($"http://{nodeUrl}");
+                HttpClient.DefaultRequestHeaders.Authorization = authValue;
+            }
+        }
+
+        public static async Task<List<string>> GetConnectionNamesAsync(int timeoutMs)
+        {
+            string responseBody = "[]";
+            int counter = 0;
+            while (responseBody.Equals("[]") && counter < timeoutMs)
+            {
+                var response = await HttpClient.GetAsync("api/connections");
+                // keep trying until there are connections
+                if (response.IsSuccessStatusCode)
+                {
+                    responseBody = await response.Content.ReadAsStringAsync();
+                }
+
+                if (responseBody.Equals("[]"))
+                    await Task.Delay(100);
+
+                counter+=100;
+            }
+
+            if (responseBody.Equals("[]"))
+                return new List<string>();
+
+            var json = JArray.Parse(responseBody);
+            var conn = json.First();
+
+            var connectionNames = new List<string>();
+            while (conn != null)
+            {
+                var name = conn["name"];
+                connectionNames.Add(name.Value<string>());
+                conn = conn.Next;
+            }
+
+            return connectionNames;
+        }
+        
+        public static async Task ForceCloseConnectionsAsync(List<string> connectionNames)
+        {
+            foreach (var name in connectionNames)
+                await DeleteAsync($"api/connections/{name}");
+        }
+
+        private static async Task DeleteAsync(string path)
+        {
+            var response = await HttpClient.DeleteAsync(path);
+            if (response.StatusCode != HttpStatusCode.NoContent)
+            {
+                throw new Exception(response.StatusCode.ToString());
+            }
+        }
+    }
+}
--- IntegrationTesting/RabbitMQTestExamples/RabbitMQTestExamples.IntegrationTests/TestMessageReceipt.cs
+++ IntegrationTesting/RabbitMQTestExamples/RabbitMQTestExamples.IntegrationTests/TestMessageReceipt.cs
@@ -2,12 +2,39 @@ using RabbitMQTestExamples.ConsoleApp;
 using RabbitMQTestExamples.IntegrationTests.Helpers;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace RabbitMQTestExamples.IntegrationTests
 {
     public class TestMessageReceipt
     {
+        [Fact]
+        public async Task ConnectionKillerExample()
+        {
+            ConnectionKiller.Initialize("localhost:15672", "guest", "guest");
+
+            // ARRANGE
+            QueueDestroyer.DeleteQueue("queueX", "/");
+            var cts = new CancellationTokenSource();
+            var fake = new FakeProcessor();
+            var myMicroservice = new Consumer(fake);
+
+            // ACT
+            myMicroservice.Consume(cts.Token, "queueX");
+
+            // put a break point here and go a look at the connections in the management ui.
+
+            Thread.Sleep(1000);
+            var connections = await ConnectionKiller.GetConnectionNamesAsync(10000);
+            await ConnectionKiller.ForceCloseConnectionsAsync(connections);
+
+            // Now go back to the management console and you will see the connection state changes to closed
+
+            // To make a test out of this, create a consumer that has auto recovery enabled and make a test that
+            // ensures it continues to consume after multiple connection failures
+        }
+
         [Fact]
         public void If_SendMessageToQueue_ThenConsumerReceiv4es()
         {
